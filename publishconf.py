d111fedb012f26b3a8cba31573f4b97099cfee70
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2013-10-21 18:05:30 +0200

    added email to footer and unset the removing output option on publish config file

--- publishconf.py
+++ publishconf.py
@@ -5,8 +5,7 @@ from __future__ import unicode_literals
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
-import sys
+import os, sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
@@ -15,7 +14,7 @@ from pelicanconf import *
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+#DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
